12cbef41c6fce8fbb5d5d3641faad3dce921725b
Florian Ducret <florian.ducret@hotmail.fr>
2016-12-18 19:13:30 +0100

    declaration CORS

--- routeIndex.js
+++ routeIndex.js
@@ -2,15 +2,14 @@
  * Created by Utilisateur on 23/11/2016.
  */
 var express = require('express');
-var cors = require('cors');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
-
 app.options('*', cors());
 
 var router = express.Router();
